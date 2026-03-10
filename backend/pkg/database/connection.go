18633f8bd52ee4679122a03572d967452e968bc6
wahyu.kurnia <wahyuk.kurnia@phiraka.com>
2026-03-10 15:11:51 +0700

    update connection

--- backend/pkg/database/connection.go
+++ backend/pkg/database/connection.go
@@ -27,7 +27,10 @@ func Connect() *gorm.DB {
 			host, user, pass, dbName, port, sslMode)
 	}
 
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  dsn,
+		PreferSimpleProtocol: true, // Disables implicit prepared statement caching (fixes SQLSTATE 42P05)
+	}), &gorm.Config{})
 	if err != nil {
 		log.Fatal("Failed to connect to database:", err)
 	}
