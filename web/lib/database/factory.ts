bc169cecc3afe6ff4b46ae381b12073de3148b72
wahyu.kurnia <wahyuk.kurnia@phiraka.com>
2026-04-27 09:37:20 +0700

    chore: remove web and web-landing folders after migration

--- web/lib/database/factory.ts
+++
@@ -1,8 +0,0 @@
-import { IAnalyticsRepository } from './interfaces/repository.interface'
-import { SupabaseAnalyticsRepository } from './repositories/supabase/analytics.repository'
-
-export class RepositoryFactory {
-  static createAnalyticsRepository(): IAnalyticsRepository {
-    return new SupabaseAnalyticsRepository()
-  }
-}
